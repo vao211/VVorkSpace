15704edb34fd236696c6e35a6d9e920eda2ebd73
vao211 <vaolu211@gmail.com>
2024-12-20 17:39:43 +0700

    + func create shortcut and optimize run on startup

--- VVorkSpace.py
+++ VVorkSpace.py
@@ -1,15 +1,20 @@
 import customtkinter as ctk
-from tkinter import messagebox
 import subprocess
+import Messagebox as msb
 import os
 from PIL import Image, ImageTk #ảnh
-import winreg as reg #auto mở
+import win32com.client #tạo shortcut
+# import winreg as reg #auto mở
 
-def run_on_startup():
+""" _run on startup
+    def run_on_startup():
     app_name = "VVork Space"
     exe_path = os.path.abspath(__file__)
-
+    
     #Registry
+    '''
+    Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
+    '''
     try:
         key = reg.OpenKey(reg.HKEY_CURRENT_USER,
                           r"Software\Microsoft\Windows\CurrentVersion\Run",
@@ -18,6 +23,77 @@ def run_on_startup():
         reg.CloseKey(key)
     except Exception as e:
         messagebox.showerror("Error", f"Failed to add to startup:\n{e}")
+"""
+    #icon
+
+
+def open_settings():
+    settings_window = ctk.CTkToplevel(app)
+    settings_window.title("Settings")
+    settings_window.geometry("640x480")
+    settings_window.resizable(False, False)
+    settings_window.grab_set()
+    # settings_window.attributes('-toolwindow', True, '-topmost', True)
+    settings_window.config(cursor="@cursor.cur", bg="black")
+    
+    screen_width = settings_window.winfo_screenwidth()
+    screen_height = settings_window.winfo_screenheight()
+    window_width = 640
+    window_height = 480
+    position_right = int(screen_width/2 - window_width/2)
+    position_down = int(screen_height/2 - window_height/2)
+    settings_window.geometry(f"{window_width}x{window_height}+{position_right}+{position_down}")
+    
+    #img
+    img_shortcut = ImageTk.PhotoImage(Image.open("icon_png/shortcut.png").resize((100,100)))
+    img_run_on_startup = ImageTk.PhotoImage(Image.open("icon_png/run_on_startup.png").resize((100,100)))
+    
+    #button shortcut
+    btn_create_shortcut = ctk.CTkButton(settings_window, text="Shortcut", 
+                                        image= img_shortcut,
+    command=lambda: create_shortcut(os.path.abspath(__file__), 
+     os.path.join(os.path.expanduser("~"),"Desktop","VVork Space.lnk")),
+                                        bg_color="black", fg_color="black",
+                                        width=20, height=2,
+                                        )
+    btn_create_shortcut.grid(row=3, column=2, padx=20, pady=20)
+    btn_create_shortcut.configure(cursor = "@cursor.cur")   
+    
+    #button run_on_startup
+    btn_run_on_startup = ctk.CTkButton(settings_window, text="Run on startup", 
+                                       image= img_run_on_startup,
+                                       command=lambda: run_on_startup(),
+                                       bg_color="black", fg_color="black",
+                                       width=20, height=2,
+                                        )
+    
+    btn_run_on_startup.grid(row=3, column=3, padx=20, pady=20)
+    btn_run_on_startup.configure(cursor = "@cursor.cur")
+    
+    
+                 
+def create_shortcut(target, shortcut_path, msg=True):
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shortcut = shell.CreateShortCut(shortcut_path)
+    shortcut.TargetPath = target
+    shortcut.WorkingDirectory = os.path.dirname(target)
+    shortcut.IconLocation = target
+    shortcut.save()
+    if msg:
+        msb.CTkMessagebox.messagebox(title="Shortcut created!", 
+                                    text=f"Shortcut created at:\n{shortcut_path}", 
+                                    sound="on", button_text="OK", size="320x150", 
+                                    center=True, top=True)
+    
+def run_on_startup():
+    create_shortcut(os.path.abspath(__file__),
+                    os.path.join(os.path.expanduser("~"),
+                                 "AppData", "Roaming", "Microsoft", "Windows",
+                                 "Start Menu", "Programs", "Startup",
+                                 "VVork Space.lnk"), msg=False)
+    msb.CTkMessagebox.messagebox(title="Run on startup!",text="VVork Space will run on startup",
+                                 sound="on", button_text="OK", 
+                                 size="320x150", center=True, top=True)
 
 def open_app(app_path):
     subprocess.Popen(f'start /MAX "" "{app_path}"', shell=True)
@@ -29,13 +105,16 @@ def app_init():
     app.resizable(False, False)
     app.iconbitmap("VVorkSpace.ico")
     
-    # bg_image = Image.open("image.png")
-    # bg_image = bg_image.resize((1280, 720), Image.LANCZOS)
-    # bg_image = ImageTk.PhotoImage(bg_image)
+    ''' _bg_image
+    bg_image = Image.open("image.png")
+    bg_image = bg_image.resize((1280, 720), Image.LANCZOS)
+    bg_image = ImageTk.PhotoImage(bg_image)
     
-    # bg_label = ctk.CTkLabel(app, image=bg_image, text="")
-    # bg_label.place(x=0, y=0, relwidth=1, relheight=1)
-    # bg_label.image = bg_image
+    bg_label = ctk.CTkLabel(app, image=bg_image, text="")
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+    bg_label.image = bg_image
+    '''    
+
     
     #giữa màn hình
     screen_width = app.winfo_screenwidth()
@@ -55,7 +134,6 @@ def app_init():
     app.grid_columnconfigure(0, weight=1)
     app.grid_columnconfigure(5, weight=1)
 
-    #icon
     img_lol = ImageTk.PhotoImage(Image.open("icon_png/lol.png").resize((100,100)))
     img_vscode = ImageTk.PhotoImage(Image.open("icon_png/vscode.png").resize((100,100)))
     img_coccoc = ImageTk.PhotoImage(Image.open("icon_png/coccoc.png").resize((100,100)))
@@ -64,7 +142,9 @@ def app_init():
     img_poe = ImageTk.PhotoImage(Image.open("icon_png/poe.png").resize((100,100)))
     img_discord = ImageTk.PhotoImage(Image.open("icon_png/discord.png").resize((100,100)))
     img_game = ImageTk.PhotoImage(Image.open("icon_png/game.png").resize((100,100)))
-    
+    img_setting = ImageTk.PhotoImage(Image.open("icon_png/setting.png").resize((100,100)))    
+
+
     #button
     
     #vscode
@@ -155,8 +235,23 @@ def app_init():
     command=lambda: open_app(r"C:\Users\NGAIVINH\Desktop\Game"))
     btn_Open_GAME.grid(row=2, column=4, padx=20, pady=20)
     btn_Open_GAME.configure(cursor = "@cursor.cur")
+ 
+ 
+    btn_settings = ctk.CTkButton(app, 
+                                 text="",
+                                 width=100, height=40,
+                                 image= img_setting,
+                                 bg_color="black",
+                                 fg_color="black",
+                                 command=lambda:open_settings())
+    btn_settings.configure(cursor = "@cursor.cur")
+    btn_settings.place(relx=1.0, 
+                       rely=0.0, 
+                       anchor="ne", 
+                       x=-10, y=10)
     
 if __name__ == "__main__":
-    run_on_startup()
+    # run_on_startup()
     app_init()
     app.mainloop()
+    
